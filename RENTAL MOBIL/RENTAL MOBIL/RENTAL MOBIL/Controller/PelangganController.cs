c42201bb6276eda45c1915ff6b02659d6a337bc9
Arie <116137224+Ariemaharrucha@users.noreply.github.com>
2023-12-12 04:46:28 -0800

    .

---
+++ RENTAL MOBIL/RENTAL MOBIL/RENTAL MOBIL/Controller/PelangganController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RENTAL_MOBIL.Controller
+{
+    public class PelangganController
+    {
+    }
+}
